53ceaa05aeaf10056b119cb4062018025f9f49e6
adigic <adigic@hotmail.com>
2023-12-02 02:02:13 +0100

    Siktar på VG nivå så la till kod som laddar game.html så jag kan fortsätta med uppgiften där.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,41 +1,43 @@
 function startGame() {
-    // Hämta användarnamnet från inputfältet
-    var usernameInput = document.getElementById("username");
-    var username = usernameInput.value;
-  
-    // Kontrollera om användarnamnet är tomt
-    if (!username) {
-      // Visa felmeddelande med en "alert" och markera input med röd ram
-      usernameInput.style.border = "2px solid red";
-  
-      // Hämta eller skapa ett element för "usernameWarning"
-      var usernameWarning = document.getElementById("usernameWarning");
-  
-      if (!usernameWarning) {
-        usernameWarning = document.createElement("p");
-        usernameWarning.id = "usernameWarning";
-        user.appendChild(usernameWarning);
-      }
-  
-      // Uppdatera innehållet och stilen för "usernameWarning"
-      usernameWarning.innerHTML = "username invalid";
-      usernameWarning.style.color = "red";
-  
-      return;
+  // Hämta användarnamnet från inputfältet
+  var usernameInput = document.getElementById("username");
+  var username = usernameInput.value;
+
+  // Kontrollera om användarnamnet är tomt
+  if (!username) {
+    // Visa felmeddelande med en "alert" och markera input med röd ram
+    usernameInput.style.border = "2px solid red";
+
+    // Hämta eller skapa ett element för "usernameWarning"
+    var usernameWarning = document.getElementById("usernameWarning");
+
+    if (!usernameWarning) {
+      usernameWarning = document.createElement("p");
+      usernameWarning.id = "usernameWarning";
+      user.appendChild(usernameWarning);
     }
-  
-    // Återställ ramen till standard om användarnamn finns
-    usernameInput.style.border = "";
-  
-    // Spara användarnamnet i localStorage
-    localStorage.setItem("username", username);
-  
-    // Ta bort startknappen och andra element
-    var startButton = document.getElementById("startButton");
-    var gameTitle = document.getElementById("gameTitle");
-    var userLogin = document.getElementById("user");
-    startButton.remove();
-    gameTitle.remove();
-    userLogin.remove();
+
+    // Uppdatera innehållet och stilen för "usernameWarning"
+    usernameWarning.innerHTML = "username invalid";
+    usernameWarning.style.color = "red";
+
+    return;
   }
-  
+
+  // Återställ ramen till standard om användarnamn finns
+  usernameInput.style.border = "";
+
+  // Spara användarnamnet i localStorage
+  localStorage.setItem("username", username);
+
+  // Ta bort startknappen och andra element
+  var startButton = document.getElementById("startButton");
+  var gameTitle = document.getElementById("gameTitle");
+  var userLogin = document.getElementById("user");
+  startButton.remove();
+  gameTitle.remove();
+  userLogin.remove();
+
+  // Ladda spelet (game.html)
+  window.location.href = "game.html";
+}
